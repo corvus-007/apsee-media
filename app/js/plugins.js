89f4efe7a4d1979abdcde964719b1b7fd1bd9985
Денис <corvus007@yandex.ru>
2018-05-17 22:35:06 +0300

    Update 2018-05-17T19:34:59.496Z

--- app/js/plugins.js
+++
@@ -1,3 +0,0 @@
-//=require ../../node_modules/svg4everybody/dist/svg4everybody.js
-//=require ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
-//=require ../../node_modules/flickity/dist/flickity.pkgd.min.js
--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   svg4everybody();
 
   $('.top-news-carousel').flickity({
-    // options
     wrapAround: true,
+    prevNextButtons: false,
+    autoPlay: true,
     contain: true,
     cellSelector: '.top-news-carousel__item'
   });
