5fba96c4f8fc984cc59ea62a5622fcbd3e6f5a6f
Denis <corvus007@yandex.ru>
2018-05-18 16:08:43 +0300

    Update 2018-05-18T13:08:39.384Z

--- js/modules.js
+++ js/modules.js
@@ -22,3 +22,15 @@ window.util = (function () {
   }
 })();
 
+window.mainNav = (function () {
+  var $ = window.jQuery;
+  var mainNav = document.querySelector('.main-nav');
+  var mainNavToggle = document.querySelector('.main-nav-toggle');
+
+  mainNavToggle.addEventListener('click', function (event) {
+    event.preventDefault();
+    $(mainNavToggle).toggleClass('main-nav-toggle--opened');
+    $(mainNav).stop().slideToggle();
+  });
+})();
+
