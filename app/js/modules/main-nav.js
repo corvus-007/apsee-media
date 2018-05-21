b950269d1f7ffc37dfd4d5be3303c72b830ad8e5
Denis <corvus007@yandex.ru>
2018-05-21 12:07:59 +0300

    Обновляет стили и скрипты в компоненте main-nav

--- app/js/modules/main-nav.js
+++ app/js/modules/main-nav.js
@@ -1,7 +1,13 @@
 window.mainNav = (function () {
+  'use strict';
+
   var $ = window.jQuery;
   var mainNav = document.querySelector('.main-nav');
   var mainNavToggle = document.querySelector('.main-nav-toggle');
+
+  if (window.matchMedia("(max-width: 991px)").matches) {
+    $(mainNav).stop().hide();
+  }
 
   mainNavToggle.addEventListener('click', function (event) {
     event.preventDefault();
