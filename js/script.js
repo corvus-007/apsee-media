513d029efda3cf256005815e9ef6039e11cd6eb6
Денис <corvus007@yandex.ru>
2018-06-01 11:42:08 +0300

    Update 2018-06-01T08:42:03.034Z

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   svg4everybody();
 
   $('.top-news-carousel').flickity({
@@ -10,4 +10,80 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   $('.enter-tabs').tabslet();
+  var $loginForm = $('#login-form').validate({
+    rules: {
+      loginPassword: {
+        minlength: 6
+      }
+    },
+    submitHandler: function (form) {
+      console.log('submit login-form');
+      var data = $(form).serializeArray();
+      var popupStatusLoginTemplate = '';
+
+      var response = $.ajax({
+        type: 'POST',
+        url: form.action,
+        data: data
+      });
+      response.done(function (responseData) {
+        console.log('done');
+        popupStatusLoginTemplate = '<div class="popup"><h2 class="popup__title">Success!</h2><p>Thank you for registering</p></div>'
+      });
+      response.fail(function (responseData) {
+        console.log('fail');
+        popupStatusLoginTemplate = '<div class="popup"><h2 class="popup__title">Something went wrong</h2><p>Please try again later.</p></div>'
+      });
+      response.always(function (responseData) {
+        console.log('always');
+        form.reset();
+        $.fancybox.close(true);
+        $.fancybox.open(popupStatusLoginTemplate);
+      });
+    }
+  });
+
+  var $signupForm = $('#signup-form').validate({
+    submitHandler: function (form) {
+      console.log('submit signup-form');
+      var data = $(form).serializeArray();
+      var popupStatusSignupTemplate = '';
+
+      var response = $.ajax({
+        type: 'POST',
+        url: form.action,
+        data: data
+      });
+      response.done(function (responseData) {
+        console.log('done');
+        popupStatusSignupTemplate = '<div class="popup"><h2 class="popup__title">Success!</h2><p>Thank you for registering</p></div>'
+      });
+      response.fail(function (responseData) {
+        console.log('fail');
+        popupStatusSignupTemplate = '<div class="popup"><h2 class="popup__title">Something went wrong</h2><p>Please try again later.</p></div>'
+      });
+      response.always(function (responseData) {
+        console.log('always');
+        form.reset();
+        $.fancybox.close(true);
+        $.fancybox.open(popupStatusSignupTemplate);
+      });
+    },
+    rules: {
+      password: {
+        minlength: 6
+      },
+      confirmPassword: {
+        minlength: 6,
+        equalTo: "#field-signup-password"
+      }
+    },
+    messages: {
+      confirmPassword: {
+        equalTo: "Please enter the same password as above"
+      }
+    }
+  });
+
+
 });
