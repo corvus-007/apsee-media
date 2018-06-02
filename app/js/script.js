8721003953169824deda29360d93f78b31caab8f
Денис <corvus007@yandex.ru>
2018-06-02 23:25:01 +0300

    Создает модальное окно с авторизацией и входом

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   svg4everybody();
+
+  $('[data-fancybox]').fancybox({
+    buttons: [
+      'close'
+    ]
+  });
+
+  $.fancybox.defaults.animationEffect = "zoom";
 
   $('.top-news-carousel').flickity({
     wrapAround: true,
@@ -7,5 +15,92 @@ document.addEventListener('DOMContentLoaded', function() {
     autoPlay: true,
     contain: true,
     cellSelector: '.top-news-carousel__item'
+  });
+
+  $('.enter-tabs').tabslet({
+    animation: true
+  });
+
+  $('.enter-tabs').on('_before', function () {
+    $('.enter-tabs').find('form').each(function (form) {
+      this.reset();
+      $loginForm.resetForm();
+      $signupForm.resetForm();
+    });
+  });
+
+  var $loginForm = $('#login-form').validate({
+    rules: {
+      loginPassword: {
+        minlength: 6
+      }
+    },
+    submitHandler: function (form) {
+      console.log('submit login-form');
+      var data = $(form).serializeArray();
+      var popupStatusLoginTemplate = '';
+
+      var response = $.ajax({
+        type: 'POST',
+        url: form.action,
+        data: data
+      });
+      response.done(function (responseData) {
+        console.log('done');
+        popupStatusLoginTemplate = '<div class="popup"><h2 class="popup__title">Success!</h2><p>Thank you for registering</p></div>'
+      });
+      response.fail(function (responseData) {
+        console.log('fail');
+        popupStatusLoginTemplate = '<div class="popup"><h2 class="popup__title">Something went wrong</h2><p>Please try again later.</p></div>'
+      });
+      response.always(function (responseData) {
+        console.log('always');
+        form.reset();
+        $.fancybox.close(true);
+        $.fancybox.open(popupStatusLoginTemplate);
+      });
+    }
+  });
+
+  var $signupForm = $('#signup-form').validate({
+    submitHandler: function (form) {
+      console.log('submit signup-form');
+      var data = $(form).serializeArray();
+      var popupStatusSignupTemplate = '';
+
+      var response = $.ajax({
+        type: 'POST',
+        url: form.action,
+        data: data
+      });
+      response.done(function (responseData) {
+        console.log('done');
+        popupStatusSignupTemplate = '<div class="popup"><h2 class="popup__title">Success!</h2><p>Thank you for registering</p></div>'
+      });
+      response.fail(function (responseData) {
+        console.log('fail');
+        popupStatusSignupTemplate = '<div class="popup"><h2 class="popup__title">Something went wrong</h2><p>Please try again later.</p></div>'
+      });
+      response.always(function (responseData) {
+        console.log('always');
+        form.reset();
+        $.fancybox.close(true);
+        $.fancybox.open(popupStatusSignupTemplate);
+      });
+    },
+    rules: {
+      password: {
+        minlength: 6
+      },
+      confirmPassword: {
+        minlength: 6,
+        equalTo: "#field-signup-password"
+      }
+    },
+    messages: {
+      confirmPassword: {
+        equalTo: "Please enter the same password as above"
+      }
+    }
   });
 });
