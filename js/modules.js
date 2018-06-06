253ece3752e60c2b6b2771444a946d6824084b08
Денис <corvus007@yandex.ru>
2018-06-06 15:39:26 +0300

    Update 2018-06-06T12:39:21.242Z

--- js/modules.js
+++ js/modules.js
@@ -92,3 +92,71 @@ window.headerSearchForm = (function () {
   });
 })();
 
+window.comments = (function () {
+  'use strict';
+
+  console.log('comments');
+
+  var $ = window.jQuery;
+  var commentsList = document.querySelector('.comments-list');
+
+  if (!commentsList) {
+    return;
+  }
+
+  var addCommentForm = document.querySelector('.add-comment-form');
+  var commentTextarea = addCommentForm.querySelector('.add-comment-form__textarea')
+  var commentTemplate = document.querySelector('#comment-template').content;
+
+  function collectionCommentDataForm(form) {
+    var dataForm = {
+      username: 'mrnobody'
+    };
+
+    var commentTextarea = form.querySelector('.add-comment-form__textarea')
+
+    dataForm.text = commentTextarea.value;
+    dataForm.commentId = 'comment-' + Math.random().toFixed(10).slice(2);
+
+    return dataForm;
+  }
+
+  addCommentForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var dataCommentForm = collectionCommentDataForm(addCommentForm);
+    commentsList.appendChild(generateCommentElement(dataCommentForm));
+  });
+
+  function generateCommentElement(data) {
+    var commentNode = commentTemplate.cloneNode(true);
+    commentNode.querySelector('.comment').dataset.commentId = data.commentId;
+    commentNode.querySelector('.comment__username').textContent = data.username;
+    commentNode.querySelector('.comment__text').textContent = data.text;
+    return commentNode;
+  }
+
+  $(commentsList).on('click', '.comment__reply', function (event) {
+    event.preventDefault();
+    var $currentComment = $(this).closest('.comment');
+    var currentCommentId = $currentComment.data('comment-id');
+    var $currentCommentBody = $currentComment.children('.comment__body');
+
+    var commentFormTemplate = addCommentForm.cloneNode(true);
+    commentFormTemplate.classList.remove('add-comment-form');
+    commentFormTemplate.classList.add('comment__reply-form');
+    var inputParentCommentId = document.createElement('input')
+    inputParentCommentId.type = 'hidden';
+    inputParentCommentId.name = 'parentCommentId';
+    inputParentCommentId.value = currentCommentId;
+    commentFormTemplate.appendChild(inputParentCommentId);
+    $currentCommentBody.append(commentFormTemplate);
+
+    commentFormTemplate.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      var dataCommentForm = collectionCommentDataForm(commentFormTemplate);
+      $(commentFormTemplate).replaceWith(generateCommentElement(dataCommentForm));
+    });
+  });
+})();
+
